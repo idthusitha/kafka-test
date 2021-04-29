21809c5650e3fccd4855b5939c2b9b4f555b7b25
thusitha <tdissanayaka@accelaero.com>
2021-04-29 09:51:00 +0200

    consumer changes

---
+++ kafka-producer/src/main/java/com/kafkatest/kafkaproducer/entity/User.java
@@ -0,0 +1,12 @@
+package com.kafkatest.kafkaproducer.entity;
+
+public class User {
+
+	private String name;
+	private int age;
+
+	public User(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
